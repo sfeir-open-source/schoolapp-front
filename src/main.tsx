9b5cb8d66bd0493530ef320bc986e94ada93979d
SantosDylan <dylan.santos@epitech.eu>
2023-03-13 19:33:53 +0100

    refactor(core): declare react query in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import ReactDOM from 'react-dom/client';
 import App from './routes/App';
 import './index.scss';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <QueryClientProvider client={queryClient}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
+  </QueryClientProvider>
 );
--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -2,18 +2,13 @@ import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Header from "./components/Header";
 import Schools from "./components/Schools";
-import { QueryClient, QueryClientProvider } from "react-query";
-
 
 export default function Home() {
-  const queryClient = new QueryClient();
 
   return (
-    <div className="p-4">
+    <div className="p-4 flex flex-col gap-4">
       <Header/>
-      <QueryClientProvider client={queryClient}>
       <Schools/>
-      </QueryClientProvider>
     </div>
   );
 }
