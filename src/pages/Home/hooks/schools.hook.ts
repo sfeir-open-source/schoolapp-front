fc61d2f133520abfc09aa15d870c462a7b6a57c4
SantosDylan <dylan.santos@epitech.eu>
2023-04-04 19:35:08 +0200

    feat(schools): fetch the date since the real api

--- src/pages/Home/hooks/schools.hook.ts
+++ src/pages/Home/hooks/schools.hook.ts
@@ -2,9 +2,15 @@ import { useQuery } from 'react-query';
 import type { School } from '../../../shared/interfaces/schools.interface';
 
 const BACKEND_URI: string = import.meta.env.VITE_BACKEND_URI;
+
+const SCHOOLS_URI = {
+  fake: '/fake-api/Schools.json',
+  api: `${BACKEND_URI}/schools/`,
+};
+
 export const useGetSchools = (status: string[]) =>
   useQuery<School[], Error>({
-    queryFn: () => fetch(`${BACKEND_URI}/schools`).then(res => res.json()),
+    queryFn: () => fetch(SCHOOLS_URI.api).then(res => res.json()),
     queryKey: 'school',
     select: schools => filterSchoolsByStatus(schools, status),
   });
